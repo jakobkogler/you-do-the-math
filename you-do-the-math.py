48b95009bc4c03e61db64b9186ddb62949c7ac09
Jakob Kogler <jakob.kogler@gmail.com>
2014-12-12 18:37:57 +0100

    Multi-core-execution

--- you-do-the-math.py
+++ you-do-the-math.py
@@ -1,5 +1,6 @@
-from itertools import product
+import itertools
 from sys import argv
+import pp
 
 def set_cycled_tuple(t, n):
     s = set()
@@ -8,28 +9,48 @@ def set_cycled_tuple(t, n):
     return s
 
 def compute_probability(n):
+    ppservers = ()
+    job_server = pp.Server(ppservers=ppservers)
+    core_count = job_server.get_ncpus()
+
     count = [0]*n
     set_A = set()
-    for A in product((0,1), repeat=n):
+    d = [dict() for i in range(core_count)]
+
+    tmp = 0
+    for A in itertools.product((0,1), repeat=n):
         if A not in set_A:
             cycled_set = set_cycled_tuple(A, n)
             cycled_count = len(cycled_set)
             set_A.update(cycled_set)
+            d[tmp%core_count][A] = cycled_count
+            tmp += 1
 
-            for B in product((-1,0,1), repeat=n):
-                for i in range(n):
-                    sum_prod = 0
-                    for j in range(n-i):
-                        sum_prod += A[j] * B[i+j]
-                    for j in range(i):
-                        sum_prod += A[n-i+j] * B[j]
-                    if sum_prod:
-                        break
-                    else:
-                        count[i] += cycled_count
+    count = [0] * n
+    jobs = [job_server.submit(core_computation,(d1,n), (), ("itertools",)) for d1 in d]
+    for job in jobs:
+        count2 = job()
+        for i in range(n):
+            count[i] += count2[i]
 
     for i in range(n):
-        print("{:2d}  {:9d} / {:10d} {:6.2%}".format(i+1, count[i], 6**n, count[i]/6**n))
+        print("{:2d}  {:10d} / {:10d} {:6.2%}".format(i+1, count[i], 6**n, count[i]/6**n))
+
+def core_computation(dict_A, n):
+    count = [0] * n
+    for A, cycled_count in dict_A.iteritems():
+        for B in itertools.product((-1,0,1), repeat=n):
+            for i in range(n):
+                sum_prod = 0
+                for j in range(n-i):
+                    sum_prod += A[j] * B[i+j]
+                for j in range(i):
+                    sum_prod += A[n-i+j] * B[j]
+                if sum_prod:
+                    break
+                else:
+                    count[i] += cycled_count
+    return count
 
 if __name__ == "__main__":
     if len(argv) > 1:
