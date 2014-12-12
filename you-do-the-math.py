970c412ea17ee2119122cf76a102ec9ce52177e9
Jakob Kogler <jakob.kogler@gmail.com>
2014-12-12 15:59:44 +0100

    Split loop and change output formating

--- you-do-the-math.py
+++ you-do-the-math.py
@@ -1,22 +1,23 @@
 from itertools import product
 from sys import argv
-from fractions import Fraction
 
 def compute_probability(n):
-    c = [0]*n
+    count = [0]*n
     for A in product((0,1), repeat=n):
         for B in product((-1,0,1), repeat=n):
             for i in range(n):
                 sum_prod = 0
-                for j in range(n):
-                    sum_prod += A[j] * B[(i+j)%n]
+                for j in range(n-i):
+                    sum_prod += A[j] * B[i+j]
+                for j in range(i):
+                    sum_prod += A[n-i+j] * B[j]
                 if sum_prod:
                     break
                 else:
-                    c[i] += 1
+                    count[i] += 1
 
     for i in range(n):
-        print("{:2d}: {:6.2%}, {}".format(i+1, c[i]/6**n, Fraction(c[i], 6**n)))
+        print("{:2d} {:9d} / {:10d} : {:6.2%}".format(i+1, count[i], 6**n, count[i]/6**n))
 
 if __name__ == "__main__":
     if len(argv) > 1:
