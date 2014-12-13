be085f6e4fc0b93ed3c751702b8fdfd73bdaa5b3
Jakob Kogler <jakob.kogler@gmail.com>
2014-12-13 14:36:30 +0100

    Filter vector B for sum1 and sum2 = 0
    Number for i = 0 is actually A081671, and is calculated with a recursion

--- you-do-the-math.py
+++ you-do-the-math.py
@@ -2,11 +2,13 @@ from itertools import product
 from sys import argv
 import pp
 
+
 def set_cycled_tuple(t, n):
     s = set()
     for i in range(n):
         s.add(tuple(t[(i+j)%n] for j in range(n)))
     return s
+
 
 def compute_probability(n):
     ppservers = ()
@@ -30,11 +32,22 @@ def compute_probability(n):
     jobs = [job_server.submit(core_computation,(d1,n), (), ()) for d1 in d]
     for job in jobs:
         count2 = job()
-        for i in range(n):
+        for i in range(1, n):
             count[i] += count2[i]
+    count[0] = oeis_A081671(n)
 
     for i in range(n):
         print("{:2d}  {:10d} / {:10d} {:6.2%}".format(i+1, count[i], 6**n, float(count[i])/6**n))
+
+
+def oeis_A081671(n):
+    if n == 0:
+        return 1
+    a, b = 1, 4
+    for i in range(2, n+1):
+        a, b = b, (4*(2*i-1)*b - 12*(i-1)*a) / i
+    return b
+
 
 def core_computation(dict_A, n):
     count = [0] * n
@@ -42,34 +55,27 @@ def core_computation(dict_A, n):
     stack = []
     # for each vector A, create all possible vectors B
     for A, cycled_count in dict_A.iteritems():
-        ones = [sum(A[i:]) for i in range(n)]
-        stack.append(([0] * n, 0, 0))
+        ones = [sum(A[i:]) for i in range(n)] + [0]
+        # + [0], so that later ones[n] doesn't throw a exception
+        stack.append(([0] * n, 0, 0, 0))
 
         while stack:
-            B, index, sum1 = stack.pop()
+            B, index, sum1, sum2 = stack.pop()
             if index < n:
                 # fill vector B[index] in all possible ways,
                 # so that it's still possible to reach 0.
-                if A[index] == 0:
-                    for v in(-1, 0, 1):
+                for v in (-1, 0, 1):
+                    sum1_new = sum1 + v * A[index]
+                    sum2_new = sum2 + v * A[index - 1 if index else n - 1]
+                    if abs(sum1_new) <= ones[index+1] and abs(sum2_new) <= ones[index] - A[n-1]:
                         C = B[:]
                         C[index] = v
-                        stack.append((C, index + 1, sum1))
-                else:
-                    if ones[index]-1 > -sum1:
-                        C = B[:]
-                        C[index] = -1
-                        stack.append((C, index + 1, sum1 - 1))
-                    if abs(sum1) <= ones[index] - 1:
-                        # already zero
-                        stack.append((B, index + 1, sum1))
-                    if sum1 < ones[index]-1:
-                        C = B[:]
-                        C[index] = 1
-                        stack.append((C, index + 1, sum1 + 1))
+                        stack.append((C, index + 1, sum1_new, sum2_new))
+
             else:
                 # B is complete, calculate the sums
-                for i in range(n):
+                count[1] += cycled_count  # we know that the sum = 0 for i = 1
+                for i in range(2, n):
                     sum_prod = 0
                     for j in range(n-i):
                         sum_prod += A[j] * B[i+j]
@@ -80,6 +86,7 @@ def core_computation(dict_A, n):
                     else:
                         count[i] += cycled_count
     return count
+
 
 if __name__ == "__main__":
     if len(argv) > 1:
