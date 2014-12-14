874a6f9d25862f334f1fbf92e5d9fc9947a7b21a
Jakob Kogler <jakob.kogler@gmail.com>
2014-12-14 19:57:08 +0100

    Revert "First try using integers (bitwise) instead of lists"
    This reverts commit 843240e1a9fe1e71d56f5cf4fc72c1fdddad1508.

--- you-do-the-math.py
+++ you-do-the-math.py
@@ -10,44 +10,26 @@ def set_cycled_tuple(t, n):
     return s
 
 
-def cycle(A, n):
-    mask = (1 << (2*n - 2)) - 1
-    return ((A & mask) << 2) | (A >> (2*n - 2))
-
-
 def compute_probability(n):
     ppservers = ()
     job_server = pp.Server(ppservers=ppservers)
     core_count = job_server.get_ncpus()
 
-
-    # generate all vectors A
-    n = 4
-    list_A = [0b00, 0b11]
-    for i in range(n-1):
-        list_A2 = []
-        for A in list_A:
-            list_A2.append((A << 2) | 0b00)
-            list_A2.append((A << 2) | 0b11)
-        list_A = list_A2
-
     # generate and divide all possible vector A (modulo cycled vectors)
-    todo = [list() for _ in range(core_count)]
-    tmp = 0
     set_A = set()
-    for A in list_A:
+    d = [dict() for i in range(core_count)]
+    tmp = 0
+    for A in product((0,1), repeat=n):
         if A not in set_A:
-            double_set = set()
-            for i in range(n):
-                A = cycle(A, n)
-                double_set.add(A)
-            set_A.update(double_set)
-            todo[tmp%core_count].append((A, len(double_set)))
+            cycled_set = set_cycled_tuple(A, n)
+            cycled_count = len(cycled_set)
+            set_A.update(cycled_set)
+            d[tmp%core_count][A] = cycled_count
             tmp += 1
 
     # start threads on each core, wait and combine results
     count = [0] * n
-    jobs = [job_server.submit(core_computation,(todo_core,n), (cycle,), ()) for todo_core in todo]
+    jobs = [job_server.submit(core_computation,(d1,n), (), ()) for d1 in d]
     for job in jobs:
         count2 = job()
         for i in range(1, n):
@@ -67,35 +49,42 @@ def oeis_A081671(n):
     return b
 
 
-def core_computation(todo, n):
+def core_computation(dict_A, n):
     count = [0] * n
-
-    #create look-up table for sum calculations
-    lookup = dict()
-    lookup[0] = 0
-    for i in range(n):
-        for k, v in list(lookup.iteritems()):
-            lookup[(k << 2) | 0b01] = v + 1
-            lookup[(k << 2) | 0b00] = v
-            lookup[(k << 2) | 0b10] = v - 1
 
     stack = []
     # for each vector A, create all possible vectors B
-    for A, cycled_count in todo:
-        stack.append((0, 0)) # empty vector, 0 entries filled
+    for A, cycled_count in dict_A.iteritems():
+        ones = [sum(A[i:]) for i in range(n)] + [0]
+        # + [0], so that later ones[n] doesn't throw a exception
+        stack.append(([0] * n, 0, 0, 0))
 
         while stack:
-            B, index = stack.pop()
+            B, index, sum1, sum2 = stack.pop()
             if index < n:
-                stack.append(((B << 2) | 0b00, index + 1))
-                stack.append(((B << 2) | 0b01, index + 1))
-                stack.append(((B << 2) | 0b10, index + 1))
+                # fill vector B[index] in all possible ways,
+                # so that it's still possible to reach 0.
+                for v in (-1, 0, 1):
+                    sum1_new = sum1 + v * A[index]
+                    sum2_new = sum2 + v * A[index - 1 if index else n - 1]
+                    if abs(sum1_new) <= ones[index+1] and abs(sum2_new) <= ones[index] - A[n-1]:
+                        C = B[:]
+                        C[index] = v
+                        stack.append((C, index + 1, sum1_new, sum2_new))
+
             else:
                 # B is complete, calculate the sums
-                for i in range(0, n):
-                    if lookup[A & B] == 0:
+                count[1] += cycled_count  # we know that the sum = 0 for i = 1
+                for i in range(2, n):
+                    sum_prod = 0
+                    for j in range(n-i):
+                        sum_prod += A[j] * B[i+j]
+                    for j in range(i):
+                        sum_prod += A[n-i+j] * B[j]
+                    if sum_prod:
+                        break
+                    else:
                         count[i] += cycled_count
-                        B = cycle(B, n)
     return count
 
 
@@ -103,4 +92,4 @@ if __name__ == "__main__":
     if len(argv) > 1:
         compute_probability(int(argv[1]))
     else:
-        compute_probability(5)
+        compute_probability(10)
