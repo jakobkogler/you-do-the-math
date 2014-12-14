99e0509c7a195342eae5c6f226f6261b12c25bc9
Jakob Kogler <jakob.kogler@gmail.com>
2014-12-14 21:39:18 +0100

    Cleaning up

--- you-do-the-math.py
+++ you-do-the-math.py
@@ -9,35 +9,40 @@ def compute_probability(n):
     job_server = pp.Server(ppservers=ppservers)
     core_count = job_server.get_ncpus()
 
+    print(core_count)
     # generate all possible vector A
     # and divide them to the cores
     visited = set()
     todo = [dict() for _ in range(core_count)]
     core_index = 0
-    for A in product((0,1), repeat=n):
+    for A in product((0, 1), repeat=n):
         if A not in visited:
             # generate all vectors, which have the same probability
             # mirrored and cycled vectors
             same_probability_set = set()
             for i in range(n):
-                tmp = [A[(i+j)%n] for j in range(n)]
+                tmp = [A[(i+j) % n] for j in range(n)]
                 same_probability_set.add(tuple(tmp))
                 same_probability_set.add(tuple(tmp[::-1]))
             visited.update(same_probability_set)
-            todo[core_index%core_count][A] = len(same_probability_set)
+            todo[core_index % core_count][A] = len(same_probability_set)
             core_index += 1
+    for t in todo:
+        print(len(t))
 
     # start threads on each core, wait and combine results
     count = [0] * n
-    jobs = [job_server.submit(core_computation,(todo_on_core,n), (), ()) \
+    jobs = [job_server.submit(core_computation, (todo_on_core, n), (), ())
             for todo_on_core in todo]
     for job in jobs:
         count2 = job()
         for i in range(1, n):
             count[i] += count2[i]
+
     count[0] = oeis_A081671(n)
 
     return count
+
 
 def oeis_A081671(n):
     if n == 0:
@@ -89,17 +94,18 @@ def core_computation(dict_A, n):
 
 
 if __name__ == "__main__":
-    n = int(argv[1]) if len(argv) > 1 else 10
+    n = int(argv[1]) if len(argv) > 1 else 11
 
     start_time = time()
     count = compute_probability(n)
     end_time = time()
     duration = int(round(end_time - start_time))
 
-    print("Calculation for n = {} took {}:{:02d} minutes\n" \
+    print("Calculation for n = {} took {}:{:02d} minutes\n"
           .format(n, duration // 60, duration % 60))
 
     l = lambda x: str(len(str(x)))
     for i in range(n):
-        print("{0:{1}d}  {2:{3}d} / {4}  {5:6.2%}" \
-              .format(i+1, l(n), count[i], l(count[0]), 6**n, float(count[i])/6**n))
+        print("{0:{1}d}  {2:{3}d} / {4}  {5:6.2%}"
+              .format(i+1, l(n), count[i], l(count[0]),
+                      6**n, float(count[i])/6**n))
